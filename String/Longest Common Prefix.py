9b764eb5ce3af9459ab14e6e2cf4ac527c3a926e
tzxdtc <tzxdtc10@gmail.com>
2020-12-15 21:59:49 +0900

    Longest Common Prefix

---
+++ String/Longest Common Prefix.py
@@ -0,0 +1,7 @@
+ if not strs: return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i,x in enumerate(s1):
+            if x != s2[i]:
+                return s2[:i]
+        return s1
